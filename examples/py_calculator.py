94a068e79e0dc22d1ddecc85a82ac7a2cd2441d2
Koichi Yatsuzuka <k.yattyan349245@gmail.com>
2026-05-16 14:30:21 +0900

    Organize examples

--- examples/py_calculator.py
+++ examples/py_calculator.py
@@ -41,6 +41,45 @@ def func(**kwargs):
     for key in kwargs.keys():
         print(kwargs[key])
 
+class Container:
+    """Python オブジェクトとして残る（tl の Tuple/List に変換されない）コンテナ。
+    __getitem__ / __setitem__ の PyObject 経由テスト用。"""
+    def __init__(self, data: list):
+        self.data = list(data)
+
+    def __getitem__(self, key: int):
+        return self.data[key]
+
+    def __setitem__(self, key: int, value):
+        self.data[key] = value
+
+    def __iter__(self):
+        return iter(self.data)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __mul__(self, n: int) -> "Container":
+        return Container(self.data * n)
+
+    def __rmul__(self, n: int) -> "Container":
+        return Container(self.data * n)
+
+    def __add__(self, other: "Container") -> "Container":
+        return Container(self.data + other.data)
+
+def make_container(data: list) -> "Container":
+    return Container(data)
+
+def sum_dict(d: dict) -> int:
+    return sum(d.values())
+
+def first_of_tuple(t: tuple):
+    return t[0]
+
+def identity_tuple(t: tuple) -> tuple:
+    return t
+
 if __name__ == "__main__":
     c = Calculator("test")
     print(c.compute("add", 1, 2))
